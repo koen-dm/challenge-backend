55e29adb9a61650b580650314d9eb6a86f5df198
koendebruijn <bruijnk@gmail.com>
2023-01-30 18:02:12 +0100

    Initial commit

---
+++ src/main/kotlin/eu/deliverymatch/challengebackend/ChallengeBackendApplication.kt
@@ -0,0 +1,11 @@
+package eu.deliverymatch.challengebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChallengeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/eu/deliverymatch/challengebackend/ChallengeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.deliverymatch.challengebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
